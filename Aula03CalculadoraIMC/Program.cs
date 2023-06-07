c9aeab0d53f7f6958a3d5e0c4ed8d9a3060a3f02
Stella Gonçalves <135771572+estelar2003@users.noreply.github.com>
2023-06-07 17:24:39 -0300

    Add files via upload

---
+++ Aula03CalculadoraIMC/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03CalculadoraIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float coxinha = 5.75f;
+            float acrescimo_catupiry = 1.00f;
+            float coxinhaCatupiry = coxinha + acrescimo_catupiry;
+
+
+            Console.WriteLine(" a coxinha custa R$ " + coxinha);
+            Console.WriteLine(" acréscimo de catupiry: R$ " + acrescimo_catupiry);
+            Console.WriteLine(" a coxinha com catupiry R$ "+ coxinhaCatupiry);
+
+            // a calculadora de IMC 
+            float peso = 70.0f;
+            float altura = 1.57f;
+            float IMC = peso / (altura * altura);
+
+            Console.WriteLine("\n seu IMC é de: "+ IMC);
+
+
+            Console.ReadKey();
+            
+
+
+
+        }
+    }
+}
